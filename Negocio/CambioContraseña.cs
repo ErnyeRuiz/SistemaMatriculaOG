714b5534abcc95636ef20e27946cfa2319619bdf
ErnyeRuiz <ernye1010@gmail.com>
2024-04-17 13:25:07 -0600

    Solucion de bug en recuperar contraseña No puede recuperar contraseña sino ha inicado sesion al menos una vez

--- Negocio/CambioContraseña.cs
+++ Negocio/CambioContraseña.cs
@@ -1,6 +1,7 @@
 ﻿using Datos;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,41 @@ namespace Negocio
         {
             ConexionBD con = new ConexionBD();
             con.CambioIngreso(cedula);
+        }
+        public string ValidarPrimerIngreso(string cedula) //Valida si un usuario ya hecho su primer ingreso
+        {
+            try
+            {
+                ConexionBD con = new ConexionBD();
+
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@CedulaEstudiante";
+                sqlParameter.Value = cedula;
+                sqlParameter.SqlDbType = System.Data.SqlDbType.VarChar;
+
+                List<SqlParameter> sqlParameters = new List<SqlParameter>
+                {
+                    sqlParameter
+                };
+
+                int result = con.ExecuteSPWithInt("VerificarPrimerIngreso", sqlParameters);
+
+                switch (result)
+                {
+                    case 0:
+                        return "No ha realizado su primer ingreso al sistema.";
+                    case 1:
+                        return "1"; //El usuario ya hizo su primer inicio de sesion
+                    case -1:
+                        return "Usuario no encontrado";
+                    default:
+                        return null;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
