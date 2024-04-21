8a66f313372690b30924806dffec668d319cd1ac
ErnyeRuiz <ernye1010@gmail.com>
2024-04-20 21:34:08 -0600

    try catch en recuperar contraseña

--- SistemaMatriculaOG/Pages/RecuperarContrasena.aspx.cs
+++ SistemaMatriculaOG/Pages/RecuperarContrasena.aspx.cs
@@ -13,57 +13,64 @@ namespace SistemaMatriculaOG
     {
         protected void BtnConfirmar_Click(object sender, EventArgs e)
         {
-
-            if (string.IsNullOrEmpty(TxtCedula.Text))
+            try
             {
-                ScriptManager.RegisterStartupScript(this, GetType(), "mostrarError", "mostrarError('Se requiere ingresar la cédula.')", true);
-                return;
-            }
-
-            if (TxtCedula.Text.Length < 9)
-            {
-                ScriptManager.RegisterStartupScript(this, GetType(), "mostrarError", "mostrarError('La cédula no cumple con la longitud mínima requerida.')", true);
-                return;
-            }
-
-            Entidades.Estudiante estudiante = Negocio.Estudiante.BuscarEstudiante(TxtCedula.Text);
-
-            string Cedula = TxtCedula.Text;
-            string NombreEstudiante = estudiante.NombreEstudiante + " " + estudiante.Apellido1 + " " + estudiante.Apellido2;
-
-            if (estudiante != null) {
-                //Validamos que haya hecho su primer inicio de sesion
-                CambioContraseña con = new CambioContraseña();
-                string EstadoPrimerIngreso = con.ValidarPrimerIngreso(Cedula);
-
-                if (EstadoPrimerIngreso == "1") //1 si ya iniciado sesion al menos una vez
+                if (string.IsNullOrEmpty(TxtCedula.Text))
                 {
-                    GeneradorContrasenias generador = new GeneradorContrasenias();
-                    string contraseniatemporal = generador.GenerarContrasenia();
-
-                    Correos correo = new Correos();
-
-
-                    con.RecuperarContra(estudiante.CedulaEstudiante);
-
-                    con.CambioContrasenia(estudiante.CedulaEstudiante, contraseniatemporal, true);
-
-                    string CorreooEnviado = correo.EnviodeCredenciales(estudiante.Correo, estudiante.CedulaEstudiante, contraseniatemporal, NombreEstudiante);
-
-                    string mensaje = "En cuanto sea respondida su solicitud de recuperación de contraseña le enviaremos sus credenciales por correo electrónico, por favor estar al pendiente de su bandeja de entrada, gracias.";
-                    ScriptManager.RegisterStartupScript(this, GetType(), "mostrarMensaje", $"mostrarMensaje('{mensaje}')", true);
-                }
-                else {
-                    ScriptManager.RegisterStartupScript(this, GetType(), "mostrarError", $"mostrarError('{EstadoPrimerIngreso}')", true);
+                    ScriptManager.RegisterStartupScript(this, GetType(), "mostrarError", "mostrarError('Se requiere ingresar la cédula.')", true);
                     return;
                 }
 
-
-
+                if (TxtCedula.Text.Length < 9)
+                {
+                    ScriptManager.RegisterStartupScript(this, GetType(), "mostrarError", "mostrarError('La cédula no cumple con la longitud mínima requerida.')", true);
+                    return;
                 }
-            else
+
+                Entidades.Estudiante estudiante = Negocio.Estudiante.BuscarEstudiante(TxtCedula.Text);
+
+                string Cedula = TxtCedula.Text;
+                string NombreEstudiante = estudiante.NombreEstudiante + " " + estudiante.Apellido1 + " " + estudiante.Apellido2;
+
+                if (estudiante != null) {
+                    //Validamos que haya hecho su primer inicio de sesion
+                    CambioContraseña con = new CambioContraseña();
+                    string EstadoPrimerIngreso = con.ValidarPrimerIngreso(Cedula);
+
+                    if (EstadoPrimerIngreso == "1") //1 si ya iniciado sesion al menos una vez
+                    {
+                        GeneradorContrasenias generador = new GeneradorContrasenias();
+                        string contraseniatemporal = generador.GenerarContrasenia();
+
+                        Correos correo = new Correos();
+
+
+                        con.RecuperarContra(estudiante.CedulaEstudiante);
+
+                        con.CambioContrasenia(estudiante.CedulaEstudiante, contraseniatemporal, true);
+
+                        string CorreooEnviado = correo.EnviodeCredenciales(estudiante.Correo, estudiante.CedulaEstudiante, contraseniatemporal, NombreEstudiante);
+
+                        string mensaje = "En cuanto sea respondida su solicitud de recuperación de contraseña le enviaremos sus credenciales por correo electrónico, por favor estar al pendiente de su bandeja de entrada, gracias.";
+                        ScriptManager.RegisterStartupScript(this, GetType(), "mostrarMensaje", $"mostrarMensaje('{mensaje}')", true);
+                    }
+                    else {
+                        ScriptManager.RegisterStartupScript(this, GetType(), "mostrarError", $"mostrarError('{EstadoPrimerIngreso}')", true);
+                        return;
+                    }
+
+
+
+                    }
+                else
+                {
+                    ScriptManager.RegisterStartupScript(this, GetType(), "mostrarError", "mostrarError('Cédula no pertenece a ningún usuario')", true);
+                    return;
+                }
+            }
+            catch (Exception)
             {
-                ScriptManager.RegisterStartupScript(this, GetType(), "mostrarError", "mostrarError('Cédula no pertenece a ningún usuario')", true);
+                ScriptManager.RegisterStartupScript(this, GetType(), "mostrarError", "mostrarError('Algo salió mal')", true);
                 return;
             }
         }
